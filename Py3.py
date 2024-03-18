cefc7767e9ca92c4f052e3719798e85a9dce7e72
Rushikesh <81320028+rushixkesh@users.noreply.github.com>
2024-03-18 22:01:20 +0530

    Create Py3.py

---
+++ Py3.py
@@ -0,0 +1,43 @@
+x = str(input("please enter a string: "))
+
+print(len(x))
+
+print(x*10)
+
+print(x[0])
+
+print(x[0:3])
+
+print(x[-3:])
+
+print(x[::-1])
+
+if len(x) >= 7:
+
+print(x[6])
+
+else:
+
+print("The string is not up to length 7")
+
+print(x[1:-1])
+
+print(x.upper())
+
+print(x.replace('a', 'e'))
+
+print(x.capitalize())
+
+print(x.title())
+
+print(x.upper())
+
+print(x.lower())
+
+print(x.find('a'))
+
+print(x.replace('h','H'))
+
+print(x.split())
+
+print(x.strip())
