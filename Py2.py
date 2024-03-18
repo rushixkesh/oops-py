0adf7f1d683295d83e11f77b6cc8da964e1ea683
Rushikesh <81320028+rushixkesh@users.noreply.github.com>
2024-03-18 22:00:27 +0530

    Create Py2.py

---
+++ Py2.py
@@ -0,0 +1,64 @@
+name=input("\nEnter the Name of Student:\n") 
+
+dept=input("\nEnter the Department of Student:\n")
+
+year=input("\nEnter the year of Studying:\n") 
+
+semester=input("\nEnter the Semester of Student:\n") 
+
+sec=input("\nEnter the Section Of Student:\n") 
+
+roll_no=int(input("\nEnter the Roll No. of Student:\n"))
+
+sub1=int(input("\nEnter the Marks obtained in Subject-1:\n")) 
+
+sub2=int(input("\nEnter the Marks obtained in Subject-2:\n"))
+
+sub3=int(input("\nEnter the Marks obtained in Subject-3:\n")) 
+
+sub4=int(input("\nEnter the Marks obtained in Subject-4:\n")) 
+
+sub5=int(input("\nEnter the Marks obtained in Subject-5:\n"))
+
+total= sub1+sub2+sub3+sub4+sub5 
+
+per= float(total/5.0)
+
+print("\n ")
+
+print("\n<<<<<<<<S.B.JAIN INSTITUTE OF 
+
+TECHNOLOGY,MNG,RE,NAGPUR>>>>>>>>\n")
+      print("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<MARKSHEET>>>>>>>>>>>>>>>>>>>>>>>>
+
+>>>")
+
+print("\n ")
+
+print("\n Name Of Student\t:", name) 
+
+print("\n Department\t\t:", dept ) 
+
+print("\n Year\t\t\t:", year)
+
+print("\n Roll No.\t\t:",roll_no ) 
+
+print("\n Section\t\t:", sec) 
+
+print("\n Semester\t\t:", semester) 
+
+print("\n Subect-1\t\t:",sub1) 
+
+print("\n Subect-2\t\t:",sub2) 
+
+print("\n Subect-3\t\t:",sub3) 
+
+print("\n Subect-4\t\t:",sub4) 
+
+print("\n Subect-5\t\t:",sub5) 
+
+print("\n Total\t\t\t:",total) 
+
+print("\n Percentage\t\t:",per)
+
+print("\n ")
